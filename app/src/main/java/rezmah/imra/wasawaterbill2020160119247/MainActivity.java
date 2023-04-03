682f40f371ea76506eccb9b5286f989d8fb37aa7
imran619 <imransanny619@gmail.com>
2023-04-03 17:18:35 +0600

    water_bill ui

---
+++ app/src/main/java/rezmah/imra/wasawaterbill2020160119247/MainActivity.java
@@ -0,0 +1,14 @@
+package rezmah.imra.wasawaterbill2020160119247;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
